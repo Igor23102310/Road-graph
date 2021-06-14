114b91a0f93c5c47d08ffd7c1b1cdec5c718ed2b
Igor23102310 <neroapple@mail.ru>
2021-06-14 23:37:54 +0300

    First commit

---
+++ variant5.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+# -*- coding: cp1251 -*-
+"""Шаблон для созадния модулей
+        для Python3"""
+
+#-------------------------------------------------------------------------------
+# Name:        Введите название модуля
+#
+# Author:
+#
+# Created:     15.02.2017
+# Copyright:   (c) Кафедра геоинформатики и кадастра ННГАСУ 2017
+#-------------------------------------------------------------------------------
+
+#import os #Импорт модулей
+import networkx as nx
+
+def main():
+    """
+    Описание
+    переменные
+    результаты
+    """
+    FileName="Исходные дороги MIF/dor5.MIF"
+
+
+
+def ReadMIF(FileName):
+    """
+    Чтение файла MIF
+    """
+
+    return(dicNodes2,dicEdges)
+
+def ReadLine(arrLines,PlineIndex):
+    """
+    Чтение координат полилинии
+    """
+ 
+    return arrPlineCoord
+
+def CreateGraph(dicNodes,dicEdges):
+    """
+    СОздание графа
+    """
+  
+    return G
+
+def ExportGraphNodesToMIF(G1,dicNodes,FileName):
+    """
+    Экспорт узлов в файл MIF
+    """
+
+
+def ExportNodesToMIF1(a,b,dicNodes,FileName):
+    """
+    Экспорт узлов в файл MIF
+    """
+ 
+
+def ExportNodesToMIF2(a,b,dicNodes,FileName):
+ 
+
+#=========================================================================================================
+if __name__ == '__main__':
+    main()
